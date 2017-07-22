a98108b4a905b57da3644aaa3dd2fd3f0c841592
icowwww <icowwww@abv.bg>
2017-07-23 00:58:56 +0300

    Added problem

---
+++ C# OOP Advanced/01. Interfaces and Abstraction/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Citizen : IPerson
+{
+    public string Name { get;}
+    public int Age { get;}
+    public Citizen(string name,int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+    
+
