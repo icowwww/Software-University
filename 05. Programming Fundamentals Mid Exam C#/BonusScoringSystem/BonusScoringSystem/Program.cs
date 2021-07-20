b5150fd6e1ceda0a250db4463eb3b549312da026
icowwww <icowwww@abv.bg>
2021-07-20 22:38:43 +0300

    fundamentals mid exam

---
+++ 05. Programming Fundamentals Mid Exam C#/BonusScoringSystem/BonusScoringSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BonusScoringSystem
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var studentsCount = int.Parse(Console.ReadLine());
+            var lecturesCount = int.Parse(Console.ReadLine());
+            var initialBonus = int.Parse(Console.ReadLine());
+
+            var highestBonus = 0.0;
+            var studentAttendances = 0;
+
+            for (var i = 0; i < studentsCount; i++)
+            {
+                var currStudentAttends = int.Parse(Console.ReadLine());
+                var currStudentBonus = (double) currStudentAttends / lecturesCount * (5 + initialBonus);
+
+                if (!(currStudentBonus >= highestBonus)) continue;
+                highestBonus = currStudentBonus;
+                studentAttendances = currStudentAttends;
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(highestBonus)}.");
+            Console.WriteLine($"The student has attended {studentAttendances} lectures.");
+        }
+    }
+}
---
+++ 05. Programming Fundamentals Mid Exam C#/MuOnline/MuOnline/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace MuOnline
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var health = 100;
+            var bitcoins = 0;
+
+            var inputList = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            var roomCount = 0;
+            foreach (var roomArgs in inputList)
+            {
+                roomCount++;
+                var roomArgsList = roomArgs.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var command = roomArgsList[0];
+                var number = int.Parse(roomArgsList[1]);
+
+                switch (command)
+                {
+                    case "potion":
+                        var amountToHeal = health + number <= 100 ? number : 100 - health;
+                        health += amountToHeal;
+                        Console.WriteLine($"You healed for {amountToHeal} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
+                        break;
+                    case "chest":
+                        bitcoins += number;
+                        Console.WriteLine($"You found {number} bitcoins.");
+                        break;
+                    default:
+                        health -= number;
+                        if (!(health <= 0))
+                        {
+                            Console.WriteLine($"You slayed {command}.");
+                            continue;
+                        }
+                        Console.WriteLine($"You died! Killed by {command}.");
+                        Console.WriteLine($"Best room: {roomCount}");
+                        return;
+                }
+            }
+
+            Console.WriteLine($"You've made it!\nBitcoins: {bitcoins}\nHealth: {health}");
+        }
+    }
+}
