552d1b1ef21eedb0f4580f01140766d6dbc4fa8c
Hristo Bonchev <icowwww@abv.bg>
2023-11-26 02:32:21 +0200

    change to const stings and change the usage of Regex class/Matches method from instance to static

--- C# Fundamentals Regex Expressions exercise/NetherRealms/NetherRealms/Program.cs
+++ C# Fundamentals Regex Expressions exercise/NetherRealms/NetherRealms/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System.Text.RegularExpressions;
 
-var healthRegex = new Regex(@"[^+\-*/.\d]");
-var damageRegex = new Regex(@"-?\d+(\.\d+)?");
-var operatorsRegex = new Regex(@"\*|\/");
+const string healthPattern = @"[^+\-*/.\d]";
+const string damagePattern = @"-?\d+(\.\d+)?";
+const string operatorsPattern = @"\*|\/";
 
 Console.ReadLine()?.Split(',', StringSplitOptions.TrimEntries).OrderBy(x => x).ToList()
     .ForEach(name =>
         {
-            var health = healthRegex.Matches(name).Select(k => (int)char.Parse(k.Value)).Sum();
-            var damage = damageRegex.Matches(name).Select(k => double.Parse(k.Value)).Sum();
-            damage = operatorsRegex.Matches(name).Select(k => k.Value)
+            var health = Regex.Matches(name, healthPattern).Select(k => (int)char.Parse(k.Value)).Sum();
+            var damage = Regex.Matches(name, damagePattern).Select(k => double.Parse(k.Value)).Sum();
+            damage = Regex.Matches(name, operatorsPattern).Select(k => k.Value)
                 .Aggregate(damage, (k, j) => j == "*" ? k * 2 : k / 2);
 
             Console.WriteLine($"{name} - {health} health, {damage:f2} damage");
